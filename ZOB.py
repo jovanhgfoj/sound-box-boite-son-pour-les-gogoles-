da5d72236b24399816ced2a36142c91ca53f5a98
jovanhgfoj <124568425+jovanhgfoj@users.noreply.github.com>
2023-05-15 10:05:13 +0200

    Add files via upload

---
+++ ZOB.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import pygame
+
+WIDTH = 400
+HEIGHT = 800
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+pygame.display.set_caption('ZOB')
+
